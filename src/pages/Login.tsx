3e6633bfe3efa2b02db2e6fdf4398e30d10671fa
Rodrigo Henrique <rodhenrc@outlook.com>
2022-07-26 00:31:08 -0300

    Dependencias instaladas e pagina de login criada

---
+++ src/pages/Login.tsx
@@ -0,0 +1,31 @@
+import styles from "../styles/Login.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faCommentDots } from "@fortawesome/free-solid-svg-icons";
+
+function Login() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <FontAwesomeIcon icon={faCommentDots} />
+        <h2>
+          WS<span>CHAT</span>
+        </h2>
+      </div>
+      <div className={styles.login}>
+        <div className={styles.form}>
+          <input type="text" placeholder="Usário" />
+          <input type="password" placeholder="Senha" />
+        </div>
+        <p className={styles.forgotPassword}>Esqueceu sua senha?</p>
+        <div className={styles.buttonLogin}>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </div>
+        <p className={styles.register}>
+          Não tem uma conta? <span>Registre-se!</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
